9b892ed8dbc32713300aa4747de313963f996935
lHUSHHl <i_vasenin01@bk.ru>
2021-05-19 17:40:46 +0500

    Add login page, locators

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPanelLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "login is not in the url"
+
+    def should_be_login_form(self):
+        self.is_element_present(
+            *LoginPanelLocators.LOGIN_FORM), \
+                    "login form is not presented"
+
+    def should_be_register_form(self):
+        self.is_element_present(
+            *LoginPanelLocators.REGISTRATION_FORM), \
+                    "registration form is not presented"
